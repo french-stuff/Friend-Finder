9e66bd63d1b4a983f417dbda344a6b99c9f5bcdc
French Stuff <legarreta.french@gmail.com>
2020-03-09 15:00:45 -0400

    re added module package

---
+++ app/data/friends.js
@@ -0,0 +1,50 @@
+var friends = [
+    {
+      name: "Emma Stone",
+      photo:
+        "https://en.wikipedia.org/wiki/Emma_Stone#/media/File:Emma_Stone_at_the_39th_Mill_Valley_Film_Festival_(cropped).jpg",
+      scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
+    },
+    {
+      name: "Jennifer Lawrence",
+      photo:
+        "https://www.imdb.com/name/nm2225369/mediaviewer/rm2555577344",
+      scores: [4, 2, 5, 1, 3, 2, 2, 1, 3, 2]
+    },
+    {
+      name: "Emma Watson",
+      photo:
+        "https://www.imdb.com/name/nm0914612/mediaviewer/rm2697969664",
+      scores: [5, 2, 2, 2, 4, 1, 3, 2, 5, 5]
+    },
+    {
+      name: "Emilia Clarke",
+      photo:
+        "https://www.imdb.com/name/nm3592338/mediaviewer/rm699918848",
+      scores: [3, 3, 4, 2, 2, 1, 3, 2, 2, 3]
+    },
+    {
+      name: "Matthew Mcconaughey",
+      photo:
+        "https://www.imdb.com/name/nm0000190/mediaviewer/rm477213952",
+      scores: [4, 3, 4, 1, 5, 2, 5, 3, 1, 4]
+    },
+    {
+      name: "Henry Cavill",
+      photo:
+        "https://www.imdb.com/name/nm0147147/mediaviewer/rm1318763008",
+      scores: [4, 3, 4, 2, 4, 3, 5, 3, 5, 4]
+    },
+    {
+      name: "Zac Efron",
+      photo: "https://www.imdb.com/name/nm1374980/mediaviewer/rm42217728",
+      scores: [4, 4, 2, 3, 2, 2, 3, 2, 4, 5]
+    }
+  ];
+  
+  module.exports = friends;
+
+
+
+
+  
